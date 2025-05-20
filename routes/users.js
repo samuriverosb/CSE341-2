e57ee9e40b25c9203a202fd34dab0ce056ec0315
Samuel Riveros <samuel.riverosb@gmail.com>
2025-05-20 14:37:03 -0400

    Enhance Swagger documentation for games and users routes: add tags and parameter descriptions for better clarity

--- routes/users.js
+++ routes/users.js
@@ -5,28 +5,53 @@ const validate = require('../validators');
 
 router.get(
   '/',
+  /* #swagger.tags = ['Users'] */
   userController.getUsers
 );
 
 router.get(
   '/:id',
+  /* #swagger.tags = ['Users'] */
   userController.getUserById
 );
 
 router.post(
   '/',
+  /* #swagger.tags = ['Users']
+     #swagger.parameters['user'] = {
+       in: 'body',
+       description: 'New user data',
+       required: true,
+       schema: { $ref: '#/definitions/User' }
+     }
+  */
   validate.createUserValidator(),
   validate.checkCreateUserData,
   userController.createUser
 );
 router.put(
   '/:id',
+  /* #swagger.tags = ['Users']
+     #swagger.parameters['id'] = {
+       in: 'path',
+       description: 'MongoDB ObjectId of the user',
+       required: true,
+       type: 'string'
+     }
+     #swagger.parameters['user'] = {
+       in: 'body',
+       description: 'Fields to update',
+       required: true,
+       schema: { $ref: '#/definitions/User' }
+     }
+  */
   validate.updateUserValidator(),
   validate.checkUpdateUserData,
   userController.updateUser
 );
 router.delete(
   '/:id',
+  /* #swagger.tags = ['Users'] */
   userController.deleteUser
 );
 
